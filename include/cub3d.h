2ca710484f9e35102c5e0e9977c77bb634107eb3
yakamay <yhakamay@student.42tokyo.jp>
2021-03-06 18:02:55 +0900

    modify: structure of some t_*
    we had to decrease the number of t_*s because mlx_*() sometimes accept only one or two parameters :(

--- include/cub3d.h
+++ include/cub3d.h
@@ -20,6 +20,11 @@
 # define FPS 30
 # define FRAME_TIME_LENGTH (1000 / FPS)
 
+#define MINIMAP_SCALE_FACTOR 1.0
+
+#define PI 3.14159265
+#define TWO_PI 6.28318530
+
 // these are based on macOS
 // if you want to use on othe OS, replace them
 # define KEY_ESC 53
@@ -28,23 +33,46 @@
 # define KEY_D 2
 # define KEY_W 13
 
+# define COLOR_R 0x00FF0000
+
+// s_data is a copy from 42 Docs
+// if you want more info, see:
+// https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html#writing-pixels-to-a-image
+typedef struct	s_data
+{
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_data;
+
 typedef struct	s_params
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	void		*mlx_ptr;
+	void		*win_ptr;
+	int			player_x;
+	int			player_y;
+	int			player_width;
+	int			player_height;
+	int			player_turn_direction; // -1 for left, +1 for right;
+	int			player_walk_direction;
+	float		player_rotation_angle;
+	float		player_walk_speed;
+	float		player_turn_speed;
 }				t_params;
 
-typedef struct	s_player
-{
-	float	x;
-	float	y;
-	float	width;
-	float	height;
-	int		turn_direction; // -1 for left, +1 for right;
-	int		walk_direction;
-	float	rotation_angle;
-	float	walk_speed;
-	float	turn_speed;
-}				t_player;
+// typedef struct	s_player
+// {
+// 	int		x;
+// 	int		y;
+// 	int		width;
+// 	int		height;
+// 	int			turn_direction; // -1 for left, +1 for right;
+// 	int			walk_direction;
+// 	float		rotation_angle;
+// 	float		walk_speed;
+// 	float		turn_speed;
+// }				t_player;
 
 #endif
