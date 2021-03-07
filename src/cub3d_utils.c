97fff3ebccbcdb74078db157f189251de0e666e8
yhakamay <yhakamay@student.42tokyo.jp>
2021-03-07 23:24:42 +0900

    modify: first parameter of my_mlx_pixel_put()
    from t_params *params to t_img *img

--- src/cub3d_utils.c
+++ src/cub3d_utils.c
@@ -1,10 +1,10 @@
 #include "cub3d.h"
 
-void my_mlx_pixel_put(t_params *params, int x, int y, int color)
+void my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char *dst;
 
-	dst = params->img.addr + (y * params->img.line_length + x * (params->img.bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
 
