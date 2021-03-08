08356363893b0b721bb02c1e7f3fa364a7ab057e
Yusuke Hakamaya <yhakamay@c1r5s3.42tokyo.jp>
2021-03-08 16:32:20 +0900

    modify: some player parameters
    from real nums to defined values
    
    e.g. player.color, player.width...

--- src/cub3d_utils.c
+++ src/cub3d_utils.c
@@ -12,12 +12,12 @@ void init_player(t_player *player)
 {
 	player->x = WINDOW_WIDTH / 2;
 	player->y = WINDOW_HEIGHT / 2;
-	player->color = 0x00FF0000;
-	player->width = 20;
-	player->height = 20;
+	player->color = PLAYER_COLOR;
+	player->width = PLAYER_DIAMETER;
+	player->height = PLAYER_DIAMETER;
 	player->turn_direction = 0;
 	player->walk_direction = 0;
 	player->rotation_angle = PI / 2;
-	player->walk_speed = 5;
-	player->turn_speed = 90 * (PI / 180);
+	player->walk_speed = PLAYER_WALK_SPEED;
+	player->turn_speed = PLAYER_TURN_SPEED;
 }
