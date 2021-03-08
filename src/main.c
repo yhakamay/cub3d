96bd27a2b3afac417ad6ebcd389fd06f2cd99c4d
Yusuke Hakamaya <yhakamay@c1r5s3.42tokyo.jp>
2021-03-08 16:27:56 +0900

    modify: data type of player parameters
    from int to float

--- src/main.c
+++ src/main.c
@@ -12,48 +12,98 @@
 
 #include "cub3d.h"
 
-void render_player(t_player *player, t_img *img)
+int Map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+void render_rect(int x, int y, int width, int height, int color, t_img *img)
 {
 	int i;
 	int j;
 
-	i = player->x;
-	while (i < player->x + player->width)
+	i = x;
+	while (i < x + width)
 	{
-		j = player->y;
-		while (j < player->y + player->height)
+		j = y;
+		while (j < y + height)
 		{
-			// mlx_pixel_put() in mlx lib is SUPER slow, so we have to create our own pixel_put() 😵
-			my_mlx_pixel_put(img, i, j, player->color);
+			my_mlx_pixel_put(img, i, j, color);
 			j++;
 		}
 		i++;
 	}
 }
 
+// void render_line(float x, float y, float len, int color, t_img *img)
+// {
+// 	float i;
+// 	float j;
+	
+// 	i = x;
+// 	while (i < )
+// }
+
+void render_minimap(t_params *params)
+{
+	int i;
+	int j;
+	int x;
+	int y;
+	int tile_color;
+
+	i = 0;
+	while (i < MAP_NUM_ROWS)
+	{
+		j = 0;
+		while (j < MAP_NUM_COLS)
+		{
+			x = j * TILE_SIZE;
+			y = i * TILE_SIZE;
+			tile_color = Map[i][j] != 0 ? 111 : 0;
+
+			render_rect(x, y, TILE_SIZE, TILE_SIZE, tile_color, &params->img);
+			j++;
+		}
+		i++;
+	}
+}
+
+
+void render_player(t_player *player, t_img *img)
+{
+	render_rect(player->x, player->y, player->width, player->height, player->color, img);
+}
+
 void render(t_params *params)
 {
 	render_player(&params->player, &params->img);
-
-	// two functions below are coming soon...
-	// render_minimap;
-	// render_view;
+	render_minimap(params);
 
 	mlx_put_image_to_window(params->mlx.mlx_ptr, params->mlx.win_ptr, params->img.img, 0, 0);
 }
 
 void move_player(t_player *player, t_img *img)
 {
-	int new_player_x;
-	int new_player_y;
-	int move_step;
+	player->rotation_angle += player->turn_direction * player->turn_speed;
 
-	move_step = player->walk_direction * player->walk_speed;
-	new_player_x = player->x + cos(player->rotation_angle) * move_step;
-	new_player_y = player->y + sin(player->rotation_angle) * move_step;
+	float move_step = player->walk_direction * player->walk_speed;
+	float new_player_x = player->x + cos(player->rotation_angle) * move_step;
+	float new_player_y = player->y + sin(player->rotation_angle) * move_step;
+
 	player->x = new_player_x;
 	player->y = new_player_y;
-	//render_player(player, img, COLOR_R);
 }
 
 int process_input(int keycode, t_params *params)
@@ -63,19 +113,28 @@ int process_input(int keycode, t_params *params)
 		mlx_destroy_window(params->mlx.mlx_ptr, params->mlx.win_ptr);
 		exit(0);
 	}
-
 	else if (keycode == KEY_W)
-		params->player.walk_direction = 1;
+		params->player.walk_direction = 1; // up
 	else if (keycode == KEY_S)
-		params->player.walk_direction = -1;
-	else if (keycode == KEY_D)
-		params->player.turn_direction = 1;
-	else if (keycode == KEY_A)
+		params->player.walk_direction = -1; // down
+	// else if (keycode == KEY_D)
+	// else if (keycode == KEY_A)
+	else if (keycode == KEY_ARROW_RIGHT)
 		params->player.turn_direction = -1;
+	else if (keycode == KEY_ARROW_LEFT)
+		params->player.turn_direction = 1;
+	else
+		return (1);
 
 	// change the parameter of t_player and render them
 	move_player(&params->player, &params->img);
+	params->player.turn_direction = 0;
 	render(params);
+
+	printf("▼▼▼▼▼\n");
+	printf("player.x: %f\n", params->player.x);
+	printf("player.y: %f\n", params->player.y);
+	printf("▲▲▲▲▲\n\n\n");
 
 	return (1);
 }
@@ -83,6 +142,9 @@ int process_input(int keycode, t_params *params)
 int main(void)
 {
 	t_params params;
+
+
+	setvbuf(stdout, (char *)NULL, _IONBF, 0);
 
 	// init some info
 	params.mlx.mlx_ptr = mlx_init();
