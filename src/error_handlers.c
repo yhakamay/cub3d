4ff9646ab6a593b1abe0716c47caa2f61b9cc130
南敦貴 <matsuki@minamiatsushitakanoMacBook-Pro.local>
2021-03-14 11:44:45 +0900

    feat(error_handlers.c): add function: no_file_err()

--- src/error_handlers.c
+++ src/error_handlers.c
@@ -6,7 +6,7 @@
 /*   By: yhakamay <yhakamay@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 18:11:56 by yhakamay          #+#    #+#             */
-/*   Updated: 2021/03/12 18:11:58 by yhakamay         ###   ########.fr       */
+/*   Updated: 2021/03/14 11:37:56 by matsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 int no_args_err(void)
 {
 	printf("Error. No arguments.\n");
+	return (0);
+}
+
+int no_file_err(void)
+{
+	printf("Error. No such file.\n");
 	return (0);
 }
