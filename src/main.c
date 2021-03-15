4ac8db44defddf58b3ff46164f4e2c2bc4e501fa
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-03-15 22:24:12 +0900

    refactor(setup()): add new function setup()
    to decrease the number of lines in main()

--- src/main.c
+++ src/main.c
@@ -12,34 +12,36 @@
 
 #include "../include/cub3d/cub3d.h"
 
+static void	setup(char *file_path, t_params *params)
+{
+	read_map(file_path, &params->map);
+	check_map(params);
+	init_g_val(params);
+	params->mlx.mlx_ptr = mlx_init();
+	params->mlx.win_ptr = mlx_new_window(
+		params->mlx.mlx_ptr,
+		params->map.window_width,
+		params->map.window_height,
+		WINDOW_TITLE);
+	init_player(&params->player, &params->map);
+	params->img.img = mlx_new_image(
+		params->mlx.mlx_ptr,
+		params->map.window_width,
+		params->map.window_height);
+	params->img.addr = mlx_get_data_addr(
+		params->img.img,
+		&params->img.bits_per_pixel,
+		&params->img.line_length,
+		&params->img.endian);
+}
+
 int			main(int argc, char **argv)
 {
 	t_params	params;
 
 	if (argc != 2)
 		return (no_args_err());
-	read_map(argv[1], &params.map);
-	check_map(&params);
-	init_g_val(&params);
-	params.mlx.mlx_ptr = mlx_init();
-	params.mlx.win_ptr = mlx_new_window(
-		params.mlx.mlx_ptr,
-		params.map.window_width,
-		params.map.window_height,
-		WINDOW_TITLE
-	);
-	init_player(&params.player, &params.map);
-	params.img.img = mlx_new_image(
-		params.mlx.mlx_ptr,
-		params.map.window_width,
-		params.map.window_height
-	);
-	params.img.addr = mlx_get_data_addr(
-		params.img.img,
-		&params.img.bits_per_pixel,
-		&params.img.line_length,
-		&params.img.endian
-	);
+	setup(argv[1], &params);
 	render_everything(&params);
 	mlx_hook(params.mlx.win_ptr, KEY_PRESSED, 1L << 0, key_pressed, &params);
 	mlx_hook(params.mlx.win_ptr, KEY_RELEASED, 1L << 0, key_released, &params);
