3afc186f5f9e2c1e83a02f65bbdf7577ae648faa
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-03-27 00:07:09 +0900

    style(move_player()): just style

--- src/input.c
+++ src/input.c
@@ -23,19 +23,16 @@ static void	move_player(t_player *p, t_img *img, t_map *map)
 	p->rotation_angle = normalize_angle(p->rotation_angle);
 	move_step_vert = p->walk_direction_vert * p->walk_speed;
 	move_step_horz = p->walk_direction_horz * p->walk_speed;
-	new_p_x = roundf(p->x + cos(p->rotation_angle) *
-				move_step_vert - sin(p->rotation_angle) *
-				move_step_horz);
-	new_p_y = roundf(p->y + sin(p->rotation_angle) *
-				move_step_vert + cos(p->rotation_angle) *
-				move_step_horz);
+	new_p_x = roundf(p->x +
+				cos(p->rotation_angle) * move_step_vert -
+				sin(p->rotation_angle) * move_step_horz);
+	new_p_y = roundf(p->y +
+				sin(p->rotation_angle) * move_step_vert +
+				cos(p->rotation_angle) * move_step_horz);
 	if (has_wall_at(new_p_x, new_p_y, map))
 		return ;
-	else
-	{
-		p->x = new_p_x;
-		p->y = new_p_y;
-	}
+	p->x = new_p_x;
+	p->y = new_p_y;
 }
 
 int			key_pressed(int keycode, t_params *params)
