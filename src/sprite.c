1763fd83a5d339350f6d0174633f605919a815f6
mhatsu <atsukin0mail@gmail.com>
2021-03-31 11:02:12 +0900

    feat(sprite.c): change program can render sprite

--- src/sprite.c
+++ src/sprite.c
@@ -25,46 +25,41 @@ static void	check_visible_sprites(t_params *params)
 
 	//変数の初期化
 	i = 0;
-
-	printf("before while loop in check_visible_sprites()\n");
 	//spriteがvisibleかどうか判定するループ
-	printf("g_num_sprites: %d\n", g_num_sprites);
 	while (i < g_num_sprites)
 	{
-		/////////////////////////////////
-		//        SERIOUS ERROR        //
-		/////////////////////////////////
 		////////spriteとの距離を更新////////
-		float distance;
-		printf("before get_distance()\n");
 		params->sprites[i].distance = get_distance(params->player.x, params->player.y, params->sprites[i].x, params->sprites[i].y);
-		/////////////////////////////////
-		//        SERIOUS ERROR        //
-		/////////////////////////////////
 
 		////////angle_sprite_playerの計算////////
 		//playerの位置からのspriteの絶対角度の導出
-		angle_sprite_player = atan2(params->sprites[i].y - params->player.y, params->sprites[i].x - params->player.x);
+		angle_sprite_player = atan2(-(params->sprites[i].y - params->player.y), params->sprites[i].x - params->player.x);
 		//atan2の戻り値をcub3dの角度の取り方に直す
 		if (angle_sprite_player < 0)
 			angle_sprite_player *= -1;
 		else
 			angle_sprite_player = 2 * PI - angle_sprite_player;
 		//playerからの相対角度に直す
+		printf("%f\n", angle_sprite_player * 180 / PI);
 		angle_sprite_player = angle_sprite_player - params->player.rotation_angle;
+		printf("%f\n", angle_sprite_player * 180 / PI);
 		//0~PIの範囲に直す
 		if (angle_sprite_player > PI)
 			angle_sprite_player -= 2 * PI;
 		else if (angle_sprite_player < -PI)
 			angle_sprite_player += 2 * PI;
+		params->sprites[i].angle = angle_sprite_player;
+		printf("%f\n", angle_sprite_player * 180 / PI);
 		angle_sprite_player = fabs(angle_sprite_player);
 
 		//visible判定
-		float	epsilon = 0.2;
-		if (angle_sprite_player <= (float)(FOV_ANGLE / 2) + epsilon)
+		//float	epsilon = 0.2;
+		
+		if (angle_sprite_player <= (float)(FOV_ANGLE / 2))
 			params->sprites[i].visible = true;
 		else
 			params->sprites[i].visible = false;
+		//printf("%d\n", params->sprites[i].visible);
 		i++;
 	}
 }
@@ -96,19 +91,56 @@ static void	sort_sprites(t_params *params)
 	}
 }
 
+static void	render_one_sprite(t_params *params, t_img *img, int x, int height)
+{
+	char	*color_addr;
+	int		color;
+	float	col;
+	int		i;
+	int		j;
+
+	col = 0;
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < height)
+		{
+			color_addr = img->addr + (int)roundf(i * img->height / height) * img->line_length + (int)roundf(j * img->width / height) * (img->bits_per_pixel / 8);
+			color = *(int *)color_addr;
+			if (params->map.window_height / 2 - height / 2 + i >= 0 && params->map.window_height / 2 - height / 2 + i < params->map.window_height && x + j >= 0 && x + j < params->map.window_width)
+			{
+				if (color != 0)
+					draw_pixel(&params->img, x + j, params->map.window_height / 2 - height / 2 + i, *(int *)(color_addr));
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 void	render_sprites(t_params *params)
 {
-	int	i = 0;
+	int		i = 0;
+	int		left_end_x;
+	int		wall_strip_height;
+	float	correct_wall_distance;
+	float	distance_to_plane;
 
-	printf("before check_visible_sprites()\n");
 	check_visible_sprites(params);
-	printf("after check_visible_sprites()\n");
 	sort_sprites(params);
-	while(i < g_num_sprites)
+	while (i < g_num_sprites)
 	{
-		printf("distance : %f\n", params->sprites[i].distance);
-		printf("visible : %d\n", params->sprites[i].visible);
-		printf("\n");
+		if (!params->sprites[i].visible)
+		{
+			i++;
+			continue ;
+		}
+		correct_wall_distance = params->sprites[i].distance * cos(params->sprites[i].angle);
+		distance_to_plane = (params->map.window_width / 2) / tan(FOV_ANGLE / 2);
+		wall_strip_height = (TILE_SIZE / correct_wall_distance) * distance_to_plane;
+		left_end_x = params->map.window_width / 2 + distance_to_plane * tan(params->sprites[i].angle) - wall_strip_height / 2;
+		render_one_sprite(params, &params->texture.sprite, left_end_x, wall_strip_height);
 		i++;
 	}
 }
