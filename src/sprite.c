c02dca1b3b83fe9239cd2d091102a3c25662a001
mhatsu <atsukin0mail@gmail.com>
2021-03-31 22:41:02 +0900

    refactor(sprite.c): fix some norminette errors (not all)

--- src/sprite.c
+++ src/sprite.c
@@ -12,39 +12,37 @@
 
 #include "../include/cub3d/cub3d.h"
 
-static void	check_visible_sprites(t_params *params)
+static void	check_visible_sprites(t_params *params,
+				t_player *player,
+				t_sprite *sprites)
 {
-	int			i;
-	float		angle_sprite_player;
+	int		i;
+	float	angle_sprite_player;
 
-	i = 0;
-	while (i < g_num_sprites)
+	i = -1;
+	while (++i < g_num_sprites)
 	{
-		params->sprites[i].distance = get_distance(
-			params->player.x,
-			params->player.y,
-			params->sprites[i].x,
-			params->sprites[i].y);
+		sprites[i].distance = get_distance(
+			player->x, player->y,
+			sprites[i].x, sprites[i].y);
 		angle_sprite_player = atan2(
-			-(params->sprites[i].y - params->player.y),
-			params->sprites[i].x - params->player.x);
+			-(sprites[i].y - player->y),
+			sprites[i].x - player->x);
 		if (angle_sprite_player < 0)
 			angle_sprite_player *= -1;
 		else
 			angle_sprite_player = 2 * PI - angle_sprite_player;
-		angle_sprite_player = angle_sprite_player - params->player.rotation_angle;
+		angle_sprite_player -= player->rotation_angle;
 		if (angle_sprite_player > PI)
 			angle_sprite_player -= 2 * PI;
 		else if (angle_sprite_player < -PI)
 			angle_sprite_player += 2 * PI;
-		params->sprites[i].angle = angle_sprite_player;
+		sprites[i].angle = angle_sprite_player;
 		angle_sprite_player = fabs(angle_sprite_player);
-		float	epsilon = 0.2;
-		if (angle_sprite_player <= (float)(FOV_ANGLE / 2) + epsilon)
-			params->sprites[i].visible = true;
+		if (angle_sprite_player <= (float)(FOV_ANGLE / 2) + 0.2)
+			sprites[i].visible = true;
 		else
-			params->sprites[i].visible = false;
-		i++;
+			sprites[i].visible = false;
 	}
 }
 
@@ -55,7 +53,6 @@ static void	sort_sprites(t_params *params)
 	t_sprite	temp;
 
 	i = 0;
-
 	while (i < g_num_sprites - 1)
 	{
 		j = g_num_sprites - 1;
@@ -87,11 +84,11 @@ static void	render_one_sprite(
 	int		j;
 
 	col = 0;
-	i = 0;
-	while (i < height)
+	i = -1;
+	while (++i < height)
 	{
-		j = 0;
-		while (j < height)
+		j = -1;
+		while (++j < height)
 		{
 			color_addr = img->addr + (int)roundf(
 				i * img->height / height) * img->line_length +
@@ -99,37 +96,32 @@ static void	render_one_sprite(
 			color = *(int *)color_addr;
 			if (params->map.window_height / 2 - height / 2 + i >= 0 &&
 				params->map.window_height / 2 - height / 2 + i < params->map.window_height &&
-				x + j >= 0 && x + j < params->map.window_width)
-			{
-				if (sprite->distance < params->rays[x + j].distance && color != 0)
-					draw_pixel(
-						&params->img,
-						x + j, params->map.window_height / 2 - height / 2 + i,
-						*(int *)(color_addr));
-			}
-			j++;
+				x + j >= 0 && x + j < params->map.window_width &&
+				sprite->distance < params->rays[x + j].distance &&
+				color != 0)
+				draw_pixel(
+					&params->img,
+					x + j, params->map.window_height / 2 - height / 2 + i,
+					*(int *)(color_addr));
 		}
-		i++;
 	}
 }
 
 void		render_sprites(t_params *params)
 {
-	int		i = 0;
+	int		i;
 	int		left_end_x;
 	int		wall_strip_height;
 	float	correct_wall_distance;
 	float	distance_to_plane;
 
-	check_visible_sprites(params);
+	i = -1;
+	check_visible_sprites(params, &params->player, params->sprites);
 	sort_sprites(params);
-	while (i < g_num_sprites)
+	while (++i < g_num_sprites)
 	{
 		if (!params->sprites[i].visible)
-		{
-			i++;
 			continue ;
-		}
 		correct_wall_distance = params->sprites[i].distance *
 								cos(params->sprites[i].angle);
 		distance_to_plane = (params->map.window_width / 2) /
@@ -145,6 +137,5 @@ void		render_sprites(t_params *params)
 			&params->texture.sprite,
 			left_end_x,
 			wall_strip_height);
-		i++;
 	}
 }
